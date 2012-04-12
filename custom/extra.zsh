62c277c179e90908e73b10c9d909e888c24159f4
Juan C. Müller <jcmuller@gmail.com>
2012-04-12 15:18:42 -0400

    Paths changed

---
+++ custom/extra.zsh
@@ -0,0 +1,5 @@
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+# Disable hostname completion
+ zstyle ':completion:*' hosts off
