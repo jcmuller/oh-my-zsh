8781ec6e84a4a45ce391765ef03e1690238f7276
Juan C. Müller <jcmuller@gmail.com>
2012-04-12 15:17:51 -0400

    Add and fix aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,19 +1,30 @@
-alias gpr="git pull --rebase"
-alias gprnf="git pull --rebase --no-ff"
-alias grm="git ls-files --deleted | xargs git rm"
-alias gitl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an, %cr)%Creset'\'' --abbrev-commit --date=relative'
-alias gitls='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an, %cr)%Creset'\'' --abbrev-commit --date=relative --max-count=5'
-
-alias rc="rails c"
-
-alias HEAD="lwp-request -m head"
 alias GET="lwp-request -m get"
+alias HEAD="lwp-request -m head"
 alias POST="lwp-request -m post"
 
-alias be="bundle exec"
-alias bi="bundle install"
-alias bu="bundle update"
+alias b="bundle"
+alias be="b exec"
+alias bec="be cucumber"
+alias ber="be rspec"
+alias bes="be spork"
+alias besc="bes cucumber"
+alias besr="bes rspec"
+alias bi="b install"
+alias biall='for i in /web/*; do (echo "$i:"; cd $i && (b check || b)); done'
+alias bu="b update"
+alias bum="bu models"
+alias bumall='for i in /web/*; do (echo "$i:"; cd $i && bum); done'
 
-alias bumall='for i in /web/*; do (echo "$i:"; cd $i && bundle update models); done'
+alias gitl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an, %cr)%Creset'\'' --abbrev-commit --date=relative'
+alias gitls='gitl --max-count=5'
+alias gpr="git pull --rebase"
 alias gprall='for i in /web/*; do (echo "$i:"; cd $i && gpr); done'
-alias  biall='for i in /web/*; do (echo "$i:"; cd $i && (bundle check || bundle install)); done'
+alias gprnf="gpr --no-ff"
+alias grm="git ls-files --deleted | xargs git rm"
+alias gs='git status'
+alias gst='gs'
+alias push='git push'
+alias gcv='git commit -v'
+alias gca='gcv -a'
+
+alias rc="rails c"
