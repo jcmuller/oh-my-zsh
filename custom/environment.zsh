38139dbf192838526605e6108fb0664b5ca7622f
Juan C. Müller <jcmuller@gmail.com>
2012-04-12 15:18:26 -0400

    Changes.

--- custom/environment.zsh
+++ custom/environment.zsh
@@ -1,10 +1,10 @@
-PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/Development/OSS/pianobar-scripts:/var/lib/gems/1.8/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/X11/sbin"
+PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/bin:$HOME/Development/OSS/pianobar-scripts:/var/lib/gems/1.8/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/X11/sbin:/usr/local/CrossPack-AVR/bin"
 export PATH
 
 #export EDITOR="mvim -f"
 export EDITOR="/usr/local/bin/vim"
 #export EDITOR='mvim -f --nomru -c "au VimLeave * !open -a iTerm"'
-export NODE_PATH=/usr/local/lib/node
+export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules"
 
 ## Add /Applications to tab completion for "open -a" and "run", which is aliased to the former.
 #if [ "`uname`" = "Darwin" ];
@@ -13,7 +13,4 @@ export NODE_PATH=/usr/local/lib/node
 #  compctl -f -x 'p[2]' -s "`/bin/ls -d1 /Applications/*/*.app ~/Applications/*.app | sed 's|^.*/\([^/]*\)\.app.*|\\1|;s/ /\\\\ /g'`" -- open
 #  alias run='open -a'
 #fi
-
-alias gs='git status' #You're welcome, Mr. Allison
-alias push='git push' #You're welcome, Mr. Allison
 
