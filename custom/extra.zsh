39bf5196ec6c62741cb31f1abb19cd9f42d19e88
Juan C. Müller <jcmuller@gmail.com>
2013-08-05 23:07:38 -0400

    No space at the beginning

--- custom/extra.zsh
+++ custom/extra.zsh
@@ -2,4 +2,4 @@ zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 # Disable hostname completion
- zstyle ':completion:*' hosts off
+zstyle ':completion:*' hosts off
